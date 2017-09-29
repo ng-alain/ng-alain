ad7f98dd081e6e269195063800c4491efa2a489f
cipchk <cipchk@qq.com>
2017-09-29 15:46:38 +0800

    chore: closes #6,closes #12,closes #13,closes #14,closes #15,closes #23

--- karma.conf.js
+++ karma.conf.js
@@ -28,6 +28,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    browserNoActivityTimeout: 60000
   });
 };
