5b7dfed71da7663a172f6a7d75ee8edaca0adfd5
cipchk <cipchk@qq.com>
2017-12-23 18:30:43 +0800

    fix(i18n): fix i18n loading order, close #138

--- src/app/core/i18n/i18n.service.ts
+++ src/app/core/i18n/i18n.service.ts
@@ -8,24 +8,25 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class I18NService implements AlainI18NService {
 
-    private _default = 'en';
+    private _default = 'zh-CN';
 
     private _langs = [
         { code: 'en', text: 'English' },
         { code: 'zh-CN', text: '中文' }
     ];
 
-    constructor(settings: SettingsService,
+    constructor(
+        settings: SettingsService,
         private nzLocalService: NzLocaleService,
         private translate: TranslateService,
-        private injector: Injector) {
+        private injector: Injector
+    ) {
         this._default = settings.layout.lang || translate.getBrowserLang();
         const lans = this._langs.map(item => item.code);
         if (!lans.includes(this._default)) {
             this._default = lans[0];
         }
         translate.addLangs(lans);
-        translate.setDefaultLang(this._default);
     }
 
     use(lang: string = null, firstLoad = true): Observable<any> {
@@ -35,13 +36,20 @@ export class I18NService implements AlainI18NService {
         if (!firstLoad) this.injector.get(Router).navigate([ '/' ]);
         return this.translate.use(lang);
     }
-
+    /** 获取语言列表 */
     getLangs() {
         return this._langs;
     }
-
+    /** 翻译 */
     fanyi(key: string) {
         return this.translate.instant(key);
     }
-
+    /** 默认语言 */
+    get defaultLang() {
+        return this._default;
+    }
+    /** 当前语言 */
+    get currentLang() {
+        return this.translate.currentLang || this.translate.getDefaultLang() || this._default;
+    }
 }
--- src/app/core/services/startup.service.ts
+++ src/app/core/services/startup.service.ts
@@ -3,7 +3,9 @@ import { Injectable, Injector } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { MenuService, SettingsService, TitleService } from '@delon/theme';
 import { ACLService } from '@delon/acl';
+import { zip } from 'rxjs/observable/zip';
 import { I18NService } from '../i18n/i18n.service';
+import { TranslateService } from '@ngx-translate/core';
 
 /**
  * 用于应用启动时
@@ -13,6 +15,7 @@ import { I18NService } from '../i18n/i18n.service';
 export class StartupService {
     constructor(
         private menuService: MenuService,
+        private translate: TranslateService,
         private i18n: I18NService,
         private settingService: SettingsService,
         private aclService: ACLService,
@@ -24,25 +27,31 @@ export class StartupService {
         // only works with promises
         // https://github.com/angular/angular/issues/15088
         return new Promise((resolve, reject) => {
-            this.httpClient.get('assets/app-data.json')
-                           .subscribe((res: any) => {
-                               // 应用信息：包括站点名、描述、年份
-                                this.settingService.setApp(res.app);
-                                // 用户信息：包括姓名、头像、邮箱地址
-                                this.settingService.setUser(res.user);
-                                // ACL：设置权限为全量
-                                this.aclService.setFull(true);
-                                // 初始化菜单
-                                this.menuService.add(res.menu);
-                                // i18n：设置默认语言
-                                this.i18n.use(this.settingService.layout.lang);
-                                // 设置页面标题的后缀
-                                this.titleService.suffix = res.app.name;
+            zip(
+                this.httpClient.get(`assets/i18n/${this.i18n.defaultLang}.json`),
+                this.httpClient.get('assets/app-data.json')
+            ).subscribe(([langData, appData]) => {
+                // setting language data
+                this.translate.setTranslation(this.i18n.defaultLang, langData);
+                this.translate.setDefaultLang(this.i18n.defaultLang);
 
-                                resolve(res);
-                            }, (err: HttpErrorResponse) => {
-                                resolve(null);
-                            });
+                // application data
+                const res: any = appData;
+                // 应用信息：包括站点名、描述、年份
+                this.settingService.setApp(res.app);
+                // 用户信息：包括姓名、头像、邮箱地址
+                this.settingService.setUser(res.user);
+                // ACL：设置权限为全量
+                this.aclService.setFull(true);
+                // 初始化菜单
+                this.menuService.add(res.menu);
+                // 设置页面标题的后缀
+                this.titleService.suffix = res.app.name;
+
+                resolve(res);
+            }, (err: HttpErrorResponse) => {
+                resolve(null);
+            });
         });
     }
 }
