d676598f14a5bebdd10bd8ea0c3f0538d3884edc
cipchk <cipchk@qq.com>
2018-03-10 22:21:38 +0800

    fix: fix nz-schema-form third widget bug

--- src/app/shared/json-schema/index.md
+++ src/app/shared/json-schema/index.md
@@ -1,1 +1,3 @@
 # 建议统一在 `widgets` 目录下自定义小部件
+
+> 注：nz-schema-form 本身提供nz-zorro-antd基础组件的全部实现，以及若干第三方组件的代码，可从[widgets-third](https://github.com/cipchk/nz-schema-form/tree/master/widgets-third)中获取并放置 `widgets` 目录下注册即可。
--- src/app/shared/json-schema/json-schema.module.ts
+++ src/app/shared/json-schema/json-schema.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 import { NzSchemaFormModule, WidgetRegistry } from 'nz-schema-form';
 
-import { TinymceWidget } from 'nz-schema-form/src/widgets-third/tinymce/tinymce.widget';
-import { UEditorWidget } from 'nz-schema-form/src/widgets-third/ueditor/ueditor.widget';
+import { TinymceWidget } from './widgets/tinymce/tinymce.widget';
+import { UEditorWidget } from './widgets/ueditor/ueditor.widget';
 
 export const SCHEMA_THIRDS_COMPONENTS = [
     TinymceWidget,
