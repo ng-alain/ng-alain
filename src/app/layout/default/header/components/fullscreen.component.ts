1d1f261f2b55be20adbae9e7737ccc52fd5b8e55
jecyhw <1147352923@qq.com>
2018-03-27 14:49:47 +0800

    fix: fix header fullscreen exit by ESC or F11 issue (#360)
    * fix:fix header fullscreen exit by ESC or F11 issue
    
    * Update fullscreen.component.ts

--- src/app/layout/default/header/components/fullscreen.component.ts
+++ src/app/layout/default/header/components/fullscreen.component.ts
@@ -12,11 +12,15 @@ export class HeaderFullScreenComponent {
 
     status = false;
 
+    @HostListener('window:resize')
+    _resize() {
+        this.status = screenfull.isFullscreen;
+    }
+
     @HostListener('click')
     _click() {
         if (screenfull.enabled) {
             screenfull.toggle();
         }
-        this.status = !screenfull.isFullscreen;
     }
 }
