b96cae2e56ac66a8d2f8ae080b31f6b84ee19700
cipchk <cipchk@qq.com>
2018-04-15 12:13:29 +0800

    fix(layout:passport): fix header vertical middle - remove link of header - add page title via router data property

--- src/app/routes/routes-routing.module.ts
+++ src/app/routes/routes-routing.module.ts
@@ -52,9 +52,9 @@ const routes: Routes = [
         path: 'passport',
         component: LayoutPassportComponent,
         children: [
-            { path: 'login', component: UserLoginComponent },
-            { path: 'register', component: UserRegisterComponent },
-            { path: 'register-result', component: UserRegisterResultComponent }
+            { path: 'login', component: UserLoginComponent, data: { title: '登录', titleI18n: 'pro-login' } },
+            { path: 'register', component: UserRegisterComponent, data: { title: '注册', titleI18n: 'pro-register' } },
+            { path: 'register-result', component: UserRegisterResultComponent, data: { title: '注册结果', titleI18n: 'pro-register-result' } }
         ]
     },
     // 单页不包裹Layout
