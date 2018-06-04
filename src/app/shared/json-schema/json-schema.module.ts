bfc3f0513a91d4d1db8bbda47091666bb17e29b0
cipchk <cipchk@qq.com>
2018-06-04 11:27:48 +0800

    feat: removed third widgets in @delon/form

--- src/app/shared/json-schema/json-schema.module.ts
+++ src/app/shared/json-schema/json-schema.module.ts
@@ -2,28 +2,23 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 import { DelonFormModule, WidgetRegistry } from '@delon/form';
 
-import { TinymceWidget } from './widgets/tinymce/tinymce.widget';
-import { UeditorWidget } from './widgets/ueditor/ueditor.widget';
+// import { TinymceWidget } from './widgets/tinymce/tinymce.widget';
+// import { UeditorWidget } from './widgets/ueditor/ueditor.widget';
 
 export const SCHEMA_THIRDS_COMPONENTS = [
-    TinymceWidget,
-    UeditorWidget
+  // TinymceWidget,
+  // UeditorWidget
 ];
 
 @NgModule({
-    declarations: SCHEMA_THIRDS_COMPONENTS,
-    entryComponents: SCHEMA_THIRDS_COMPONENTS,
-    imports: [
-        SharedModule,
-        DelonFormModule.forRoot()
-    ],
-    exports: [
-        ...SCHEMA_THIRDS_COMPONENTS
-    ]
+  declarations: SCHEMA_THIRDS_COMPONENTS,
+  entryComponents: SCHEMA_THIRDS_COMPONENTS,
+  imports: [SharedModule, DelonFormModule.forRoot()],
+  exports: [...SCHEMA_THIRDS_COMPONENTS],
 })
 export class JsonSchemaModule {
-    constructor(widgetRegistry: WidgetRegistry) {
-        widgetRegistry.register(TinymceWidget.KEY, TinymceWidget);
-        widgetRegistry.register(UeditorWidget.KEY, UeditorWidget);
-    }
+  constructor(widgetRegistry: WidgetRegistry) {
+    // widgetRegistry.register(TinymceWidget.KEY, TinymceWidget);
+    // widgetRegistry.register(UeditorWidget.KEY, UeditorWidget);
+  }
 }
