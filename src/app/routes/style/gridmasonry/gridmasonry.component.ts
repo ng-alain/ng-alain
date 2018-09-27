95c278e8674080da3af55cc4d1c3fa5125ae8974
cipchk <cipchk@qq.com>
2018-09-27 15:37:27 +0800

    remove box-placeholder

--- src/app/routes/style/gridmasonry/gridmasonry.component.ts
+++ src/app/routes/style/gridmasonry/gridmasonry.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-gridmasonry',
   templateUrl: './gridmasonry.component.html',
+  styleUrls: ['./gridmasonry.component.less'],
 })
 export class GridMasonryComponent {}
