d3ceefc5665d7bebb82133080b490c5c95696737
cipchk <cipchk@qq.com>
2018-10-03 20:30:34 +0800

    need mock

--- scripts/_ci/deploy.sh
+++ scripts/_ci/deploy.sh
@@ -4,6 +4,9 @@ set -u -e -o pipefail
 
 cd $(dirname $0)/../..
 
+ROOT_DIR="$(pwd)"
+DIST_DIR="$(pwd)/dist"
+
 VERSION=$(node -p "require('./package.json').version")
 
 echo "Start build version: ${VERSION}"
@@ -12,6 +15,10 @@ echo ""
 echo "Generate color less"
 echo ""
 node ./scripts/color-less.js
+
+echo '===== need mock'
+sed -i "s/const MOCK_MODULES = !environment.production/const MOCK_MODULES = true/g" ${ROOT_DIR}/src/app/delon.module.ts
+sed -i "s/if (!environment.production)/if (true)/g" ${ROOT_DIR}/src/app/layout/default/default.component.ts
 
 echo ""
 echo "Build angular"
