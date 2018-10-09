6b8168f98388549bbcfc9462d95638f881d8b84c
卡色 <cipchk@qq.com>
2018-10-09 21:37:18 +0800

    feat(theme:alain-default): refactor layout component (#714)
    BREAKING CHANGE:
    + `layout-fixed` was changed to `alain-default__fixed`
    + `layout-boxed` was changed to `alain-default__boxed`
    + `aside-collapsed` was changed to `alain-default__collapsed`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,7 @@
-import {
-  Component,
-  HostBinding,
-  OnInit,
-  Renderer2,
-  ElementRef,
-} from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
-import { SettingsService, TitleService } from '@delon/theme';
+import { TitleService } from '@delon/theme';
 import { VERSION as VERSION_ALAIN } from '@delon/theme';
 import { VERSION as VERSION_ZORRO, NzModalService } from 'ng-zorro-antd';
 
@@ -16,27 +10,9 @@ import { VERSION as VERSION_ZORRO, NzModalService } from 'ng-zorro-antd';
   template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent implements OnInit {
-  @HostBinding('class.layout-fixed')
-  get isFixed() {
-    return this.settings.layout.fixed;
-  }
-  @HostBinding('class.layout-boxed')
-  get isBoxed() {
-    return this.settings.layout.boxed;
-  }
-  @HostBinding('class.aside-collapsed')
-  get isCollapsed() {
-    return this.settings.layout.collapsed;
-  }
-  @HostBinding('class.color-weak')
-  get isColorWeak() {
-    return this.settings.layout.colorWeak;
-  }
-
   constructor(
     el: ElementRef,
     renderer: Renderer2,
-    private settings: SettingsService,
     private router: Router,
     private titleSrv: TitleService,
     private modalSrv: NzModalService,
