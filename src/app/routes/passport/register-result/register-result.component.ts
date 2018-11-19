41d0174b1f767c4a32b4f552b250720af49f2356
卡色 <cipchk@qq.com>
2018-11-19 14:12:46 +0800

    feat: add i18n of passport pages (#777)

--- src/app/routes/passport/register-result/register-result.component.ts
+++ src/app/routes/passport/register-result/register-result.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { NzMessageService } from 'ng-zorro-antd';
+import { ActivatedRoute } from '@angular/router';
+import { I18NService } from '@core/i18n/i18n.service';
 
 @Component({
   selector: 'passport-register-result',
-  templateUrl: './register-result.component.html'
+  templateUrl: './register-result.component.html',
 })
 export class UserRegisterResultComponent {
-  constructor(public msg: NzMessageService) {}
+  email = '';
+  constructor(
+    route: ActivatedRoute,
+    i18n: I18NService,
+    public msg: NzMessageService,
+  ) {
+    this.email = i18n.fanyi('app.register-result.msg', {
+      email: route.snapshot.queryParams.email || 'ng-alain@example.com',
+    });
+  }
 }
