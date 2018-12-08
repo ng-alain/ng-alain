2490497278879ed15a2f9d64a4efc7a3906e90ba
cipchk <cipchk@qq.com>
2018-12-08 15:14:40 +0800

    chore: removed uedito config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -52,23 +52,10 @@ const I18NSERVICE_PROVIDES = [
 // #endregion
 
 // #region global third module
-import { UEditorModule } from 'ngx-ueditor';
-import { NgxTinymceModule } from 'ngx-tinymce';
+
 const GLOBAL_THIRD_MODULES = [
-  UEditorModule.forRoot({
-    // **注：** 建议使用本地路径；以下为了减少 ng-alain 脚手架的包体大小引用了CDN，可能会有部分功能受影响
-    js: [
-      `//apps.bdimg.com/libs/ueditor/1.4.3.1/ueditor.config.js`,
-      `//apps.bdimg.com/libs/ueditor/1.4.3.1/ueditor.all.min.js`,
-    ],
-    options: {
-      UEDITOR_HOME_URL: `//apps.bdimg.com/libs/ueditor/1.4.3.1/`,
-    },
-  }),
-  NgxTinymceModule.forRoot({
-    baseURL: '//cdn.bootcss.com/tinymce/4.7.4/',
-  }),
 ];
+
 // #endregion
 
 // #region JSON Schema form (using @delon/form)
