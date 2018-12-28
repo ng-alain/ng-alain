9eaee1226b6755859fb2d389e68d9940402a3744
cipchk <cipchk@qq.com>
2018-12-28 13:47:34 +0800

    chore: rethrow

--- src/app/routes/routes-routing.module.ts
+++ src/app/routes/routes-routing.module.ts
@@ -89,7 +89,8 @@ const routes: Routes = [
     RouterModule.forRoot(
       routes, {
         useHash: environment.useHash,
-        // NOTICE: If you use `reuse-tab` component and turn on keepingScroll you must set to `disabled`
+        // NOTICE: If you use `reuse-tab` component and turn on keepingScroll you can set to `disabled`
+        // Pls refer to https://ng-alain.com/components/reuse-tab
         scrollPositionRestoration: 'top',
       }
     )],
