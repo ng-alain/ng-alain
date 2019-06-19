0bd4445c4ea70160aeac228aa47be61bd898e2dd
cipchk <cipchk@qq.com>
2019-06-19 12:22:44 +0800

    chore: remove core-js

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -21,12 +21,6 @@
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
-// Evergreen browsers require these.
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
-// IE Need
-import 'core-js/es7/array';
-
 /**
  * Required to support Web Animations `@angular/animation`.
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
