98b66ab1f0c44cf99fb826f64b940a7e089217e6
cipchk <cipchk@qq.com>
2019-06-23 10:52:45 +0800

    chore: fix build

--- scripts/_ci/delon.sh
+++ scripts/_ci/delon.sh
@@ -5,6 +5,7 @@ set -e
 cd $(dirname $0)/../..
 
 echo "Download latest @delon version"
+rm -rf delon-builds
 git clone --depth 1 https://github.com/ng-alain/delon-builds.git
 rm -rf node_modules/@delon
 rm -rf node_modules/ng-alain
