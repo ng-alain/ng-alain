89bbc0009ba6892f452fe8a1c23a1964d1d131ce
cipchk <cipchk@qq.com>
2019-06-23 16:47:05 +0800

    chore: update delon version

--- src/app/delon.module.ts
+++ src/app/delon.module.ts
@@ -1,6 +1,6 @@
 /**
  * 进一步对基础模块的导入提炼
- * 有关模块注册指导原则请参考：https://github.com/ng-alain/ng-alain/issues/180
+ * 有关模块注册指导原则请参考：https://ng-alain.com/docs/module
  */
 import { NgModule, Optional, SkipSelf, ModuleWithProviders } from '@angular/core';
 import { throwIfAlreadyLoaded } from '@core';
