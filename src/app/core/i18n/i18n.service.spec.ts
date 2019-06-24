7877253374860a89831a6cf5f310235d4a187516
cipchk <cipchk@qq.com>
2019-06-24 17:34:18 +0800

    chore: fix test

--- src/app/core/i18n/i18n.service.spec.ts
+++ src/app/core/i18n/i18n.service.spec.ts
@@ -16,6 +16,7 @@ describe('Service: I18n', () => {
   };
   const MockNzI18nService = {
     setLocale: () => {},
+    setDateLocale: () => {},
   };
   const MockDelonLocaleService = {
     setLocale: () => {},
