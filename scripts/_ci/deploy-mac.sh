e44dac63ebef9bf60b60dfacce38940038128498
cipchk <cipchk@qq.com>
2019-06-28 22:43:16 +0800

    chore: update breaking change of ng-zorro

--- scripts/_ci/deploy-mac.sh
+++ scripts/_ci/deploy-mac.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# bash ./scripts/_ci/deploy-mac.sh -gh -dr
+# bash ./scripts/_ci/deploy-mac.sh -dr -gh
 
 set -e
 
@@ -26,33 +26,33 @@ VERSION=$(node -p "require('./package.json').version")
 
 echo "Start build version: ${VERSION}"
 
-echo ""
-echo "Generate color less"
-echo ""
-node ./scripts/color-less.js
-
-echo '===== need mock'
-sed -i '' 's/const MOCK_MODULES = !environment.production/const MOCK_MODULES = true/g' ${ROOT_DIR}/src/app/delon.module.ts
-sed -i '' 's/if (!environment.production)/if (true)/g' ${ROOT_DIR}/src/app/layout/default/default.component.ts
-
 if [[ ${DAY_RELEASE} == true ]]; then
   bash ./scripts/_ci/delon.sh
 fi
 
-echo ""
-echo "Build angular"
-echo ""
-
-if [[ ${GH} == true ]]; then
-  node --max_old_space_size=5120 ./node_modules/@angular/cli/bin/ng build --prod --base-href /ng-alain/
-else
-  node --max_old_space_size=5120 ./node_modules/@angular/cli/bin/ng build --prod
-fi
-cp -f ${DIST_DIR}/index.html ${DIST_DIR}/404.html
-
 if [[ ${GH} == true ]]; then
   echo ""
-  echo "Deploy by gh-pages"
+  echo "===== Generate color less"
+  echo ""
+  node ./scripts/color-less.js
+
+  echo '===== need mock'
+  sed -i '' 's/const MOCK_MODULES = !environment.production/const MOCK_MODULES = true/g' ${ROOT_DIR}/src/app/delon.module.ts
+  sed -i '' 's/if (!environment.production)/if (true)/g' ${ROOT_DIR}/src/app/layout/default/default.component.ts
+
+  echo ""
+  echo "===== Build angular"
+  echo ""
+
+  if [[ ${GH} == true ]]; then
+    node --max_old_space_size=5120 ./node_modules/@angular/cli/bin/ng build --prod --base-href /ng-alain/
+  else
+    node --max_old_space_size=5120 ./node_modules/@angular/cli/bin/ng build --prod
+  fi
+  cp -f ${DIST_DIR}/index.html ${DIST_DIR}/404.html
+
+  echo ""
+  echo "===== Deploy by gh-pages"
   echo ""
   $(npm bin)/gh-pages -d dist
 fi
