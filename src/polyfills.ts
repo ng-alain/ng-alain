52650fe14f638a261ac7550c6c59cba655f95087
bingyun <578210452@qq.com>
2019-07-10 15:42:59 +0800

    add ie support  In  production environment

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,6 +18,8 @@
  * BROWSER POLYFILLS
  */
 
+//import 'core-js/es/array'; 
+
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
