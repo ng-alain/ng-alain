ec9124805c6d2cbfd8852da6a17f5bed8bbfb856
卡色 <cipchk@qq.com>
2019-08-21 12:02:00 +0800

    docs: update custom theme variables (#1299)
    - close #1292

--- scripts/color-less.js
+++ scripts/color-less.js
@@ -4,15 +4,22 @@ const {
   generateTheme
 } = require('antd-theme-generator');
 
+// Specify the name of the theme variables to be changed, default is `@primary-color`
+// Can be set all antd & ng-alain custom theme variables
+const themeVariables = [
+  '@primary-color'
+];
+
 const root = path.resolve(__dirname, '../');
 const tmpVarFilePath = path.join(root, 'scripts/var.less');
 const outputFilePath = path.join(root, './src/assets/alain-default.less');
+
 const options = {
   stylesDir: path.join(root, './src'),
   antdStylesDir: path.join(root, './node_modules/ng-zorro-antd'),
   varFile: path.join(root, './scripts/var.less'),
   mainLessFile: path.join(root, './src/styles.less'),
-  themeVariables: ['@primary-color'],
+  themeVariables,
   outputFilePath,
 };
 
