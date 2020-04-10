07d8bbf4e6f33a83075620eee33c2df49395089e
cipchk <cipchk@qq.com>
2020-04-10 16:10:32 +0800

    chore: fix

--- src/app/routes/pro/account/center/projects/projects.component.ts
+++ src/app/routes/pro/account/center/projects/projects.component.ts
@@ -1,3 +1,4 @@
+import { NzMessageService } from 'ng-zorro-antd/message';
 import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { _HttpClient } from '@delon/theme';
 
@@ -10,11 +11,16 @@ import { _HttpClient } from '@delon/theme';
 export class ProAccountCenterProjectsComponent {
   listLoading = true;
   list: any[] = [];
-  constructor(private http: _HttpClient, private cdr: ChangeDetectorRef) {
+
+  constructor(private http: _HttpClient, private msg: NzMessageService, private cdr: ChangeDetectorRef) {
     this.http.get('/api/list', { count: 8 }).subscribe((res: any) => {
       this.list = res;
       this.listLoading = false;
       this.cdr.detectChanges();
     });
+  }
+
+  suc(id: number): void {
+    this.msg.success(`标题：${id}`);
   }
 }
