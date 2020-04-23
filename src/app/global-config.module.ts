2e616fe987e0eab611ff1bbe2c5913c9596cf7f0
cipchk <cipchk@qq.com>
2020-04-24 02:37:46 +0800

    chore: updaet

--- src/app/global-config.module.ts
+++ src/app/global-config.module.ts
@@ -1,7 +1,3 @@
-/**
- * 进一步对基础模块的导入提炼
- * 有关模块注册指导原则请参考：https://ng-alain.com/docs/module
- */
 import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { throwIfAlreadyLoaded } from '@core';
 
@@ -15,7 +11,7 @@ import { AlainConfig, AlainThemeModule, ALAIN_CONFIG } from '@delon/theme';
 import { DelonMockModule } from '@delon/mock';
 import { environment } from '@env/environment';
 import * as MOCKDATA from '../../_mock';
-const alainMockModues = true ? [DelonMockModule.forRoot({ data: MOCKDATA })] : [];
+const alainMockModues = !environment.production ? [DelonMockModule.forRoot({ data: MOCKDATA })] : [];
 // #endregion
 
 // #region reuse-tab
