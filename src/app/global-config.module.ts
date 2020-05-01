b8a7afc4fcb92b37b42c660a39533a2b9e1a4f2b
卡色 <cipchk@qq.com>
2020-05-01 17:53:15 +0800

    docs: update reuse-tab (#1612)

--- src/app/global-config.module.ts
+++ src/app/global-config.module.ts
@@ -32,12 +32,13 @@ if (!environment.production) {
 // #region reuse-tab
 /**
  * 若需要[路由复用](https://ng-alain.com/components/reuse-tab)需要：
- * 1、增加 `REUSETAB_PROVIDES`
- * 2、在 `src/app/layout/default/default.component.html` 修改：
+ * 1、在 `shared-delon.module.ts` 导入 `ReuseTabModule` 模块
+ * 2、注册 `RouteReuseStrategy`
+ * 3、在 `src/app/layout/default/default.component.html` 修改：
  *  ```html
  *  <section class="alain-default__content">
- *    <reuse-tab></reuse-tab>
- *    <router-outlet></router-outlet>
+ *    <reuse-tab #reuseTab></reuse-tab>
+ *    <router-outlet (activate)="reuseTab.activate($event)"></router-outlet>
  *  </section>
  *  ```
  */
