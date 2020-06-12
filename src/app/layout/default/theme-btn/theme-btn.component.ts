14d0de9410a786cf136a60ea8f9eefec622dc78a
cipchk <cipchk@qq.com>
2020-06-12 19:59:18 +0800

    fix(theme-btn): `update` instead of `set`

--- src/app/layout/default/theme-btn/theme-btn.component.ts
+++ src/app/layout/default/theme-btn/theme-btn.component.ts
@@ -25,7 +25,7 @@ export class LayoutThemeBtnComponent implements OnInit {
   }
 
   private updateChartTheme(): void {
-    this.configSrv.update<AlainChartConfig, 'chart'>('chart', { theme: this.theme === 'dark' ? 'dark' : '' });
+    this.configSrv.set('chart', { theme: this.theme === 'dark' ? 'dark' : '' });
   }
 
   onThemeChange(theme: SiteTheme): void {
