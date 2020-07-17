6a5ee97d01c41aa492b78917317b499035d6111c
cipchk <cipchk@qq.com>
2020-07-17 13:56:19 +0800

    feat: add search network request demo
    - close #1772

--- src/app/layout/default/header/components/search.component.ts
+++ src/app/layout/default/header/components/search.component.ts
@@ -1,28 +1,52 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, HostBinding, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  HostBinding,
+  Input,
+  OnDestroy,
+} from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'header-search',
   template: `
-    <nz-input-group [nzAddOnBeforeIcon]="focus ? 'arrow-down' : 'search'">
+    <nz-input-group [nzPrefix]="iconTpl" [nzSuffix]="loadingTpl">
+      <ng-template #iconTpl>
+        <i nz-icon [nzType]="focus ? 'arrow-down' : 'search'"></i>
+      </ng-template>
+      <ng-template #loadingTpl>
+        <i *ngIf="loading" nz-icon nzType="loading"></i>
+      </ng-template>
       <input
+        type="text"
         nz-input
         [(ngModel)]="q"
+        [nzAutocomplete]="auto"
+        (input)="search($event)"
         (focus)="qFocus()"
         (blur)="qBlur()"
-        [placeholder]="'menu.search.placeholder' | translate"
+        [attr.placeholder]="'menu.search.placeholder' | translate"
       />
     </nz-input-group>
+    <nz-autocomplete nzBackfill #auto>
+      <nz-auto-option *ngFor="let i of options" [nzValue]="i">{{ i }}</nz-auto-option>
+    </nz-autocomplete>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderSearchComponent implements AfterViewInit {
+export class HeaderSearchComponent implements AfterViewInit, OnDestroy {
   q: string;
-
   qIpt: HTMLInputElement;
+  options: string[] = [];
+  search$ = new BehaviorSubject('');
+  loading = false;
 
   @HostBinding('class.alain-default__search-focus')
   focus = false;
-
   @HostBinding('class.alain-default__search-toggled')
   searchToggled = false;
 
@@ -36,18 +60,36 @@ export class HeaderSearchComponent implements AfterViewInit {
     setTimeout(() => this.qIpt.focus(), 300);
   }
 
-  constructor(private el: ElementRef) {}
+  constructor(private el: ElementRef<HTMLElement>, private cdr: ChangeDetectorRef) {}
 
-  ngAfterViewInit() {
-    this.qIpt = (this.el.nativeElement as HTMLElement).querySelector('.ant-input') as HTMLInputElement;
+  ngAfterViewInit(): void {
+    this.qIpt = this.el.nativeElement.querySelector('.ant-input') as HTMLInputElement;
+    this.search$.pipe(debounceTime(500), distinctUntilChanged()).subscribe((value) => {
+      this.options = value ? [value, value + value, value + value + value] : [];
+      this.loading = false;
+      this.cdr.detectChanges();
+    });
   }
 
-  qFocus() {
+  qFocus(): void {
     this.focus = true;
   }
 
-  qBlur() {
+  qBlur(): void {
     this.focus = false;
     this.searchToggled = false;
+  }
+
+  search(ev: KeyboardEvent): void {
+    if (ev.key === 'Enter') {
+      return;
+    }
+    this.loading = true;
+    this.search$.next((ev.target as HTMLInputElement).value);
+  }
+
+  ngOnDestroy(): void {
+    this.search$.complete();
+    this.search$.unsubscribe();
   }
 }
