230ec8ebed68f24d207330b787e0c6c3077d3652
cipchk <cipchk@qq.com>
2020-08-02 23:42:09 +0800

    chore: fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
-import { TitleService, VERSION as VERSION_ALAIN, _HttpClient } from '@delon/theme';
+import { TitleService, VERSION as VERSION_ALAIN } from '@delon/theme';
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { VERSION as VERSION_ZORRO } from 'ng-zorro-antd/version';
 import { filter } from 'rxjs/operators';
