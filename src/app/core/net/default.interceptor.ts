155e9171c2881c4ca04017c4d0edf913aceaf678
cipchk <cipchk@qq.com>
2020-08-03 17:14:07 +0800

    chore: update cors

--- src/app/core/net/default.interceptor.ts
+++ src/app/core/net/default.interceptor.ts
@@ -200,7 +200,10 @@ export class DefaultInterceptor implements HttpInterceptor {
         break;
       default:
         if (ev instanceof HttpErrorResponse) {
-          console.warn('未可知错误，大部分是由于后端不支持CORS或无效配置引起', ev);
+          console.warn(
+            '未可知错误，大部分是由于后端不支持跨域CORS或无效配置引起，请参考 https://ng-alain.com/docs/server 解决跨域问题',
+            ev,
+          );
         }
         break;
     }
