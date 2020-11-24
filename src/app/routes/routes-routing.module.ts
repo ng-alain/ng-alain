9ea8b92f4a289451782a3c25278b19a0f6e6bced
cipchk <cipchk@qq.com>
2020-11-24 18:08:08 +0800

    chore: bump angular from 10 to 11

--- src/app/routes/routes-routing.module.ts
+++ src/app/routes/routes-routing.module.ts
@@ -34,21 +34,21 @@ const routes: Routes = [
       { path: 'dashboard/workplace', component: DashboardWorkplaceComponent },
       {
         path: 'widgets',
-        loadChildren: () => import('./widgets/widgets.module').then(m => m.WidgetsModule),
+        loadChildren: () => import('./widgets/widgets.module').then((m) => m.WidgetsModule),
       },
-      { path: 'style', loadChildren: () => import('./style/style.module').then(m => m.StyleModule) },
-      { path: 'delon', loadChildren: () => import('./delon/delon.module').then(m => m.DelonModule) },
-      { path: 'extras', loadChildren: () => import('./extras/extras.module').then(m => m.ExtrasModule) },
-      { path: 'pro', loadChildren: () => import('./pro/pro.module').then(m => m.ProModule) },
+      { path: 'style', loadChildren: () => import('./style/style.module').then((m) => m.StyleModule) },
+      { path: 'delon', loadChildren: () => import('./delon/delon.module').then((m) => m.DelonModule) },
+      { path: 'extras', loadChildren: () => import('./extras/extras.module').then((m) => m.ExtrasModule) },
+      { path: 'pro', loadChildren: () => import('./pro/pro.module').then((m) => m.ProModule) },
       // Exception
-      { path: 'exception', loadChildren: () => import('./exception/exception.module').then(m => m.ExceptionModule) },
+      { path: 'exception', loadChildren: () => import('./exception/exception.module').then((m) => m.ExceptionModule) },
     ],
   },
   // 全屏布局
   {
     path: 'data-v',
     component: LayoutFullScreenComponent,
-    children: [{ path: '', loadChildren: () => import('./data-v/data-v.module').then(m => m.DataVModule) }],
+    children: [{ path: '', loadChildren: () => import('./data-v/data-v.module').then((m) => m.DataVModule) }],
   },
   // passport
   {
