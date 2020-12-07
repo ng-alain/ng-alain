b578a9d8c08013fc5960117c17410b0ead06db2f
cipchk <cipchk@qq.com>
2020-12-07 20:29:33 +0800

    chore: update

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,8 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { GlobalFooterModule } from '@delon/abc/global-footer';
+import { NoticeIconModule } from '@delon/abc/notice-icon';
 import { LayoutDefaultModule } from '@delon/theme/layout-default';
 import { SettingDrawerModule } from '@delon/theme/setting-drawer';
 import { ThemeBtnModule } from '@delon/theme/theme-btn';
-import { SharedModule } from '@shared';
+import { TranslateModule } from '@ngx-translate/core';
+import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
 
 import { LayoutBasicComponent } from './basic/basic.component';
 import { HeaderClearStorageComponent } from './basic/widgets/clear-storage.component';
@@ -33,7 +47,26 @@ import { LayoutPassportComponent } from './passport/passport.component';
 const PASSPORT = [LayoutPassportComponent];
 
 @NgModule({
-  imports: [SharedModule, ThemeBtnModule, SettingDrawerModule, LayoutDefaultModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    ThemeBtnModule,
+    SettingDrawerModule,
+    LayoutDefaultModule,
+    TranslateModule,
+    NoticeIconModule,
+    GlobalFooterModule,
+    NzDropDownModule,
+    NzInputModule,
+    NzAutocompleteModule,
+    NzGridModule,
+    NzFormModule,
+    NzSpinModule,
+    NzBadgeModule,
+    NzAvatarModule,
+    NzIconModule,
+  ],
   declarations: [...COMPONENTS, ...HEADERCOMPONENTS, ...PASSPORT],
   exports: [...COMPONENTS, ...PASSPORT],
 })
