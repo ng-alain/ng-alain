29099dc630f6ae94b9452251e5f47441a04cadb3
cipchk <cipchk@qq.com>
2020-12-22 14:59:50 +0800

    fix: fix unsupported queryParams

--- src/app/routes/passport/register/register.component.ts
+++ src/app/routes/passport/register/register.component.ts
@@ -115,8 +115,8 @@ export class UserRegisterComponent implements OnDestroy {
     }
 
     const data = this.form.value;
-    this.http.post('/register', data).subscribe(() => {
-      this.router.navigateByUrl('/passport/register-result', /* Removed unsupported properties by Angular migration: queryParams. */ {});
+    this.http.post('/register?_allow_anonymous=true', data).subscribe(() => {
+      this.router.navigate(['passport', 'register-result'], { queryParams: { email: data.mail } });
     });
   }
 
