d83aab48c4fc3918dada5fb87dc92384f476d61e
卡色 <cipchk@qq.com>
2021-02-27 20:20:57 +0800

    perf(mock): import method of mock data to make the production tree-shaking reduced 300kb (#1941)

--- scripts/_ci/deploy-pipelines.sh
+++ scripts/_ci/deploy-pipelines.sh
@@ -43,7 +43,8 @@ echo ""
 npm run theme
 
 echo '===== need mock'
-sed -i 's/if (!environment.production)/if (true)/g' ${ROOT_DIR}/src/app/global-config.module.ts
+cp -f ${ROOT_DIR}/src/environments/environment.ts ${ROOT_DIR}/src/environments/environment.prod.ts
+sed -i 's/production: false/production: true/g' ${ROOT_DIR}/src/environments/environment.prod.ts
 sed -i 's/showSettingDrawer = !environment.production;/showSettingDrawer = true;/g' ${ROOT_DIR}/src/app/layout/basic/basic.component.ts
 
 if [[ ${GH} == true ]]; then
