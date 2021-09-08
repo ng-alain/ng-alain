442e2626f26475c1a16ced9f59f56bdaa40e91ff
cipchk <cipchk@qq.com>
2021-09-08 16:11:20 +0800

    docs: add test widget demo, close #2081

--- src/app/shared/json-schema/json-schema.module.ts
+++ src/app/shared/json-schema/json-schema.module.ts
@@ -2,23 +2,17 @@ import { NgModule } from '@angular/core';
 import { DelonFormModule, WidgetRegistry } from '@delon/form';
 
 import { SharedModule } from '../shared.module';
+import { TestWidget } from './test/test.widget';
 
-// import { TinymceWidget } from './widgets/tinymce/tinymce.widget';
-// import { UeditorWidget } from './widgets/ueditor/ueditor.widget';
-
-export const SCHEMA_THIRDS_COMPONENTS = [
-  // TinymceWidget,
-  // UeditorWidget
-];
+export const SCHEMA_THIRDS_COMPONENTS = [TestWidget];
 
 @NgModule({
   declarations: SCHEMA_THIRDS_COMPONENTS,
   imports: [SharedModule, DelonFormModule.forRoot()],
-  exports: [...SCHEMA_THIRDS_COMPONENTS]
+  exports: SCHEMA_THIRDS_COMPONENTS
 })
 export class JsonSchemaModule {
   constructor(widgetRegistry: WidgetRegistry) {
-    // widgetRegistry.register(TinymceWidget.KEY, TinymceWidget);
-    // widgetRegistry.register(UeditorWidget.KEY, UeditorWidget);
+    widgetRegistry.register(TestWidget.KEY, TestWidget);
   }
 }
