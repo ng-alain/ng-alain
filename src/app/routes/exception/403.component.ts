b968b40b741b08f420e7006954f3ab61156ce107
cipchk <cipchk@qq.com>
2021-11-17 23:51:00 +0800

    perf: optimize error handling and navigate to the exception page
    - close #2132

--- src/app/routes/exception/403.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'exception-403',
-  template: ` <exception type="403" style="min-height: 500px; height: 80%;"></exception> `
-})
-export class Exception403Component {}
--- src/app/routes/exception/500.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'exception-500',
-  template: ` <exception type="500" style="min-height: 500px; height: 80%;"></exception> `
-})
-export class Exception500Component {}
