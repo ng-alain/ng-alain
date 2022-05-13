702a436d2c5a6e6041eca908064fce5a7bd278fe
esme-marie <esmemarie.sultan@gmail.com>
2022-05-13 09:41:35 +0800

    Integrate update setting api with order setting table

--- src/app/routes/dashboard/order-setting/order-setting.component.ts
+++ src/app/routes/dashboard/order-setting/order-setting.component.ts
@@ -1,21 +1,22 @@
+import { Location } from '@angular/common';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { STColumn, STComponent, STData } from '@delon/abc/st';
 import { NzMessageService } from 'ng-zorro-antd/message';
 
-interface ItemData {
-  id: number;
-  exchange: string;
-  status: number;
-  value: [
-    {
-      symbol: string;
-      bid: string;
-      ask: string;
-      amount: string;
-      interval: string;
-    }
-  ];
+interface UpdatedData {
+  exchangeId: number;
+  status: string;
+  values: string;
+}
+
+interface UpdatedValues {
+  symbol: string;
+  bid: string;
+  ask: string;
+  amount: string;
+  interval: string;
 }
 
 @Component({
@@ -23,201 +24,154 @@ interface ItemData {
   templateUrl: './order-setting.component.html'
 })
 export class OrderSettingComponent implements OnInit {
-  editCache: { [key: number]: { edit: boolean; data: ItemData } } = {};
   list: any;
   valuelist: any;
-  listOfData: ItemData[] = [];
+  updatevalue: any;
+  values: any[] = [];
+  orders: STData[] = [];
+  updatedData: UpdatedData = {
+    exchangeId: 0,
+    status: '',
+    values: ''
+  };
+  updatedValues: UpdatedValues = {
+    symbol: '',
+    bid: '',
+    ask: '',
+    amount: '',
+    interval: ''
+  };
 
-  startEdit(id: number): void {
-    this.editCache[id].edit = true;
-  }
-
-  cancelEdit(id: number): void {
-    const index = this.listOfData.findIndex(item => item.id === id);
-    this.editCache[id] = {
-      data: { ...this.listOfData[index] },
-      edit: false
-    };
-  }
-
-  saveEdit(id: number): void {
-    const index = this.listOfData.findIndex(item => item.id === id);
-    Object.assign(this.listOfData[index], this.editCache[id].data);
-    this.editCache[id].edit = false;
-  }
-
-  updateEditCache(): void {
-    this.listOfData.forEach(item => {
-      this.editCache[item.id] = {
-        edit: false,
-        data: { ...item }
-      };
-    });
-  }
-
-  // export class OrderSettingComponent {
-  // @ViewChild('st') private st!: STComponent;
-  constructor(private msg: NzMessageService, private http: HttpClient) {}
+  @ViewChild('st') private st!: STComponent;
+  constructor(private msg: NzMessageService, private http: HttpClient, public _router: Router, public _location: Location) {}
   headers = new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('access_token')}`);
+
   ngOnInit(): void {
+    this.getOrderSetting();
+  }
+
+  oColumns: STColumn[] = [
+    { title: 'Exchange', type: 'date', index: 'exchange', render: 'exchangeTpl', sort: { compare: (a, b) => a.cp.localeCompare(b.cp) } },
+    { title: 'Symbol', type: 'date', index: 'symbol', render: 'symbolTpl', sort: { compare: (a, b) => a.symbol.localeCompare(b.symbol) } },
+    { title: 'Bid', type: 'date', index: 'bid', render: 'bidTpl', sort: true },
+    { title: 'Ask', type: 'date', index: 'ask', render: 'askTpl', sort: true },
+    { title: 'Amount', type: 'date', index: 'amount', render: 'amountTpl', sort: true },
+    { title: 'Interval', type: 'date', index: 'interval', render: 'intervalTpl', sort: true },
+    {
+      title: 'Action',
+      type: 'date',
+      buttons: [
+        {
+          icon: 'edit',
+          iif: i => !i.edit,
+          click: i => this.updateEdit(i, true)
+        },
+        {
+          text: 'Save',
+          iif: i => i.edit,
+          pop: {
+            title: 'Save changes?',
+            okType: 'primary'
+          },
+          click: i => {
+            this.submit(i);
+          }
+        },
+        {
+          text: 'Cancel',
+          iif: i => i.edit,
+          click: i => this.updateEdit(i, false)
+        }
+      ]
+    }
+  ];
+
+  getOrderSetting() {
     console.log(localStorage.getItem('access_token'));
     this.http
       .get('http://localhost:3000/api/v1/setting/get-exchange-setting?_allow_anonymous=true', { headers: this.headers })
       .subscribe(res => {
-        console.log(res);
+        // console.log(res);
         this.list = res;
 
-        this.list.data.map((item: { value: any }) => {
-          console.log('item', JSON.parse(item.value));
+        this.list.data.map((item: any) => {
+          // console.log('item', JSON.parse(item.value));
           this.valuelist = JSON.parse(item.value);
+          let exchangeId = item.id;
+          let exchange = item.exchange;
+          let status = item.status;
+          let i = 0;
+          this.valuelist.map((item: any) => {
+            this.valuelist[i].valueId = (i + 1).toString();
+            this.valuelist[i].exchangeId = exchangeId;
+            this.valuelist[i].exchange = exchange;
+            this.valuelist[i].status = status;
+            // console.log('valuelist item', item);
+            i++;
+          });
           item.value = this.valuelist;
+          this.orders = item.value;
+          // console.log('listOfData', this.orders);
         });
-        this.listOfData = this.list.data;
-        console.log('valuelist', this.listOfData);
       });
-    this.updateEditCache();
   }
 
-  // orders: STData[] = [
-  //   {
-  //     id: 1,
-  //     exchange: 'Bitflyer',
-  //     status: 'A',
-  //     value: [
-  //       {
-  //         symbol: 'BTC',
-  //         bid: 1000000,
-  //         ask: 1000000,
-  //         amount: 0.001,
-  //         interval: 1
-  //       },
-  //       {
-  //         symbol: 'ETH',
-  //         bid: 1000000,
-  //         ask: 1000000,
-  //         amount: 0.001,
-  //         interval: 1
-  //       },
-  //       {
-  //         symbol: 'XRP',
-  //         bid: 1000000,
-  //         ask: 1000000,
-  //         amount: 0.001,
-  //         interval: 1
-  //       }
-  //     ]
-  // }
-  //   cp: 'Bitflyer',
-  //   symbol: 'BTC',
-  //   bid: 0,
-  //   ask: 0,
-  //   amount: 1,
-  //   interval: 0.5
-  // },
-  // {
-  //   cp: 'Okex',
-  //   symbol: 'BTC',
-  //   bid: 100,
-  //   ask: 100,
-  //   amount: 0.2,
-  //   interval: 1
-  // },
-  // {
-  //   cp: 'Huobi',
-  //   symbol: 'BTC',
-  //   bid: 100,
-  //   ask: 100,
-  //   amount: 0.2,
-  //   interval: 1
-  // },
-  // {
-  //   cp: 'Liquid',
-  //   symbol: 'BTC',
-  //   bid: 100,
-  //   ask: 100,
-  //   amount: 0.2,
-  //   interval: 1
-  // },
-  // {
-  //   cp: 'CoinCheck',
-  //   symbol: 'BTC',
-  //   bid: 100,
-  //   ask: 100,
-  //   amount: 1,
-  //   interval: 1
-  // },
-  // {
-  //   cp: 'CG',
-  //   symbol: 'BTC',
-  //   bid: 100,
-  //   ask: 100,
-  //   amount: 1,
-  //   interval: 1
-  // }
-  // ];
-  //   oColumns: STColumn[] = [
-  //     { title: 'Exchange', index: 'exchange', sort: { compare: (a, b) => a.cp.localeCompare(b.cp) } },
-  //     {
-  //       title: 'Symbol',
-  //       index: 'value',
-  //       sort: { compare: (a, b) => a.symbol.localeCompare(b.symbol) }
-  //     },
-  //     { title: 'Bid', index: 'value.bid', sort: true },
-  //     { title: 'Ask', index: 'value.ask', sort: true },
-  //     { title: 'Amount', index: 'value.amount', sort: true },
-  //     { title: 'Interval', index: 'value.interval', sort: true },
-  //     {
-  //       title: 'Action',
-  //       buttons: [
-  //         {
-  //           // text: 'Update',
-  //           icon: 'edit',
-  //           iif: i => !i.edit,
-  //           click: i => this.updateEdit(i, true)
-  //         },
-  //         {
-  //           // text: 'Delete',
-  //           icon: 'delete',
-  //           iif: i => !i.edit,
-  //           pop: {
-  //             title: 'Are you sure?',
-  //             okType: 'danger'
-  //           },
-  //           click: i => this.deleteRow(i)
-  //         },
-  //         {
-  //           text: 'Save',
-  //           iif: i => i.edit,
-  //           click: i => {
-  //             this.submit(i);
-  //           }
-  //         },
-  //         {
-  //           text: 'Cancel',
-  //           iif: i => i.edit,
-  //           click: i => this.updateEdit(i, false)
-  //         }
-  //       ]
-  //     }
-  //   ];
+  refresh(): void {
+    this._router.navigateByUrl('/refresh', { skipLocationChange: true }).then(() => {
+      // console.log(decodeURI(this._location.path()));
+      this._router.navigate([decodeURI(this._location.path())]);
+    });
+  }
 
-  //   private submit(i: STData): void {
-  //     this.msg.success(JSON.stringify(this.st.pureItem(i)));
-  //     this.updateEdit(i, false);
-  //   }
+  private submit(i: STData): void {
+    this.updatevalue = this.st.pureItem(i);
+    // console.log('updatevalue', this.updatevalue.valueId);
+    const updatedOrders = this.orders.map((item: any) => {
+      if (item.valueId == this.updatevalue.valueId) {
+        return {
+          ...item,
+          symbol: this.updatevalue.symbol,
+          bid: this.updatevalue.bid,
+          ask: this.updatevalue.ask,
+          amount: this.updatevalue.amount,
+          interval: this.updatevalue.interval,
+          status: this.updatevalue.status
+        };
+      }
+      return item;
+    });
+    this.updatedData.exchangeId = this.updatevalue.exchangeId;
+    this.updatedData.status = this.updatevalue.status;
+    // console.log('updatedOrders: ', updatedOrders, 'updatedData: ', this.updatedData);
 
-  //   private updateEdit(i: STData, edit: boolean): void {
-  //     this.st.setRow(i, { edit }, { refreshSchema: true });
-  //   }
+    updatedOrders.map((item: any) => {
+      this.updatedValues = { symbol: '', bid: '', ask: '', amount: '', interval: '' };
+      this.updatedValues.symbol = item.symbol;
+      this.updatedValues.bid = item.bid;
+      this.updatedValues.ask = item.ask;
+      this.updatedValues.amount = item.amount;
+      this.updatedValues.interval = item.interval;
+      // console.log(this.updatedValues);
+      this.values.push(this.updatedValues);
+    });
+    // console.log('updatedData ', this.updatedData);
+    this.updatedData.values = JSON.stringify(this.values);
+    // console.log('final updatedData: ', this.updatedData);
 
-  //   private deleteRow(i: STData): void {
-  //     this.st.removeRow(i);
-  //   }
-  // }
-  // function subscribe(arg0: (res: any) => void) {
-  //   throw new Error('Function not implemented.');
-  // }
-  // function item(item: any) {
-  //   throw new Error('Function not implemented.');
-  // }
+    this.http
+      .post('http://localhost:3000/api/v1/setting/update-exchange-setting?_allow_anonymous=true', this.updatedData, {
+        headers: this.headers
+      })
+      .subscribe(res => {
+        console.log(res);
+      });
+    this.msg.success('Changes saved!');
+    this.updateEdit(i, false);
+    this.getOrderSetting();
+    this.refresh();
+  }
+
+  private updateEdit(i: STData, edit: boolean): void {
+    this.st.setRow(i, { edit }, { refreshSchema: true });
+  }
 }
