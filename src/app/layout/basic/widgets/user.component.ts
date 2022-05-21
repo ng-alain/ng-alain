3acc95b179ba4490ebb2d097f683c0b701295c6b
esme-marie <esmemarie.sultan@gmail.com>
2022-05-21 21:33:23 +0800

    Added user email address on header

--- src/app/layout/basic/widgets/user.component.ts
+++ src/app/layout/basic/widgets/user.component.ts
@@ -10,24 +10,11 @@ import { Logout } from 'src/app/core/store/global.action';
   selector: 'header-user',
   template: `
     <div class="alain-default__nav-item d-flex align-items-center px-sm" nz-dropdown nzPlacement="bottomRight" [nzDropdownMenu]="userMenu">
-      <nz-avatar [nzSrc]="user.avatar" nzSize="small" class="mr-sm"></nz-avatar>
-      {{ user['username'] }}
+      <nz-avatar nzIcon="user" nzSize="small" class="mr-sm"></nz-avatar>
+      {{ user['emailAddress'] }}
     </div>
     <nz-dropdown-menu #userMenu="nzDropdownMenu">
       <div nz-menu class="width-sm">
-        <!-- <div nz-menu-item routerLink="/pro/account/center">
-          <i nz-icon nzType="user" class="mr-sm"></i>
-          {{ 'menu.account.center' | i18n }}
-        </div>
-        <div nz-menu-item routerLink="/pro/account/settings">
-          <i nz-icon nzType="setting" class="mr-sm"></i>
-          {{ 'menu.account.settings' | i18n }}
-        </div>
-        <div nz-menu-item routerLink="/exception/trigger">
-          <i nz-icon nzType="close-circle" class="mr-sm"></i>
-          {{ 'menu.account.trigger' | i18n }}
-        </div>
-        <li nz-menu-divider></li> -->
         <div nz-menu-item (click)="logout()">
           <i nz-icon nzType="logout" class="mr-sm"></i>
           {{ 'menu.account.logout' | i18n }}
