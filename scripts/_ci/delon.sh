8a61dbd1caee44f50de749789e5150c970e6411c
卡色 <cipchk@qq.com>
2023-01-09 12:33:26 +0800

    chore: bump angular to `15` (#2350)

--- scripts/_ci/delon.sh
+++ scripts/_ci/delon.sh
@@ -9,7 +9,7 @@ cd $(dirname $0)/../..
 echo "Download latest @delon version"
 rm -rf delon-builds
 git clone --depth 1 https://github.com/ng-alain/delon-builds.git
-# rm -rf node_modules/@delon
+rm -rf node_modules/@delon
 # rm -rf node_modules/ng-alain
 rsync -am delon-builds/ node_modules/
 NG_ALAIN_VERSION=$(node -p "require('./node_modules/ng-alain/package.json').version")
