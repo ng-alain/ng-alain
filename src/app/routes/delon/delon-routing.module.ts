56f6ded37218ed008a762cd942bb7912321b2af1
cipchk <cipchk@qq.com>
2023-07-21 18:59:24 +0800

    fix(*): remove deprecated guard

--- src/app/routes/delon/delon-routing.module.ts
+++ src/app/routes/delon/delon-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ACLGuard } from '@delon/acl';
+import { aclCanActivate } from '@delon/acl';
 
 import { ACLComponent } from './acl/acl.component';
 import { CacheComponent } from './cache/cache.component';
@@ -35,13 +35,13 @@ const routes: Routes = [
       {
         path: 'auth',
         component: GuardAuthComponent,
-        canActivate: [ACLGuard],
+        canActivate: [aclCanActivate],
         data: { guard: 'user1' }
       },
       {
         path: 'admin',
         component: GuardAdminComponent,
-        canActivate: [ACLGuard],
+        canActivate: [aclCanActivate],
         data: { guard: 'admin' }
       }
     ]
--- src/app/routes/routes-routing.module.ts
+++ src/app/routes/routes-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { startPageGuard } from '@core';
-import { SimpleGuard } from '@delon/auth';
+import { authSimpleCanActivate, authSimpleCanActivateChild } from '@delon/auth';
 import { PreloadOptionalModules } from '@delon/theme';
 import { environment } from '@env/environment';
 
@@ -13,8 +13,8 @@ const routes: Routes = [
   {
     path: '',
     component: LayoutBasicComponent,
-    canActivate: [startPageGuard, SimpleGuard],
-    canActivateChild: [SimpleGuard],
+    canActivate: [startPageGuard, authSimpleCanActivate],
+    canActivateChild: [authSimpleCanActivateChild],
     data: {},
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
