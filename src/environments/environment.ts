86b55da9ba124789a6e7ac0905f8428e4e78db2a
卡色 <cipchk@qq.com>
2023-10-30 15:18:51 +0800

    feat: add `@_mock` path (#2432)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,9 @@
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import * as MOCKDATA from '@_mock';
 import { DelonMockModule } from '@delon/mock';
 import { Environment } from '@delon/theme';
-
-import * as MOCKDATA from '../../_mock';
 
 export const environment = {
   production: false,
