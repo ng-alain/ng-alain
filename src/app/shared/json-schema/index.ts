d5ed3311b573eed2056fb93b18bcc9f85aafe5dd
卡色 <cipchk@qq.com>
2024-03-11 14:19:32 +0800

    docs: non-built-in widget registration method of sf (#2485)

--- src/app/shared/json-schema/index.ts
+++ src/app/shared/json-schema/index.ts
@@ -1,5 +1,10 @@
 import type { SFWidgetProvideConfig } from '@delon/form';
+// import { withCascaderWidget } from '@delon/form/widgets/cascader';
 
 import { TestWidget } from './test/test.widget';
 
-export const SF_WIDGETS: SFWidgetProvideConfig[] = [{ KEY: TestWidget.KEY, type: TestWidget }];
+export const SF_WIDGETS: SFWidgetProvideConfig[] = [
+  { KEY: TestWidget.KEY, type: TestWidget }
+  // Non-built-in widget registration method
+  // withCascaderWidget()
+];
