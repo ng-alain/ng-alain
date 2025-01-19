5d1dc6a196ab048f796ed5fbdbf871cb878197d0
cipchk <cipchk@qq.com>
2025-01-19 17:53:39 +0800

    chore: remove `shared.module.ts`

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -5,7 +5,6 @@ export * from './utils/yuan';
 
 // Module
 export * from './shared-imports';
-export * from './shared.module';
 export * from './json-schema/index';
 export * from './st-widget/index';
 export * from './cell-widget/index';
