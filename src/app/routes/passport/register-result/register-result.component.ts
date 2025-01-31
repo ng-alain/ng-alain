d67e9746dc0947dab38c4ddd5361e3d66721a9d2
cipchk <cipchk@qq.com>
2025-01-31 12:44:11 +0800

    chore: fix test

--- src/app/routes/passport/register-result/register-result.component.ts
+++ src/app/routes/passport/register-result/register-result.component.ts
@@ -3,11 +3,12 @@ import { RouterLink } from '@angular/router';
 import { I18nPipe } from '@delon/theme';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzResultModule } from 'ng-zorro-antd/result';
 
 @Component({
   selector: 'passport-register-result',
   templateUrl: './register-result.component.html',
-  imports: [RouterLink, I18nPipe, NzButtonModule]
+  imports: [RouterLink, I18nPipe, NzButtonModule, NzResultModule]
 })
 export class UserRegisterResultComponent {
   readonly msg = inject(NzMessageService);
