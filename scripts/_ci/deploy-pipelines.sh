611dabe6169e9be6200fa6291b24381507bc3f49
cipchk <cipchk@qq.com>
2025-07-07 17:38:12 +0800

    chore: fix ic

--- scripts/_ci/deploy-pipelines.sh
+++ scripts/_ci/deploy-pipelines.sh
@@ -55,6 +55,6 @@ else
   node --max_old_space_size=5120 ./node_modules/@angular/cli/bin/ng build
 fi
 
-cp -f ${DIST_DIR}/browser/index.html ${DIST_DIR}/browser/404.html
+cp -f ${DIST_DIR}/ng-alain/browser/index.html ${DIST_DIR}/ng-alain/browser/404.html
 
 echo "Finished"
