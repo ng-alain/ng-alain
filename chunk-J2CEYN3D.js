7c320ea1b779ab4455409c63103fbc53f1ddc174
cipchk <cipchk@users.noreply.github.com>
2025-10-19 02:51:32 +0000

    Automated deployment: Sun Oct 19 02:51:32 UTC 2025 09fe67c717065ea631fc088b3725a98979151b40

---
+++ chunk-J2CEYN3D.js
@@ -0,0 +1,1 @@
+import{W as o}from"./chunk-7PXCOFTV.js";import{Pa as i,fa as n}from"./chunk-3VS2Y7FB.js";var y=(()=>{class e{srv=n(o);transform(r,t){return this.srv.format(r,t)}static \u0275fac=function(t){return new(t||e)};static \u0275pipe=i({name:"price",type:e,pure:!0})}return e})();export{y as a};
