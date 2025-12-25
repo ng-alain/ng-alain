12da296309a02eb0b18d77df01f9044c2de6dc0e
卡色 <cipchk@qq.com>
2025-12-25 17:44:44 +0800

    style: fix lint config (#2588)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,7 +27,6 @@ export default tseslint.config(
     languageOptions: {
       parserOptions: {
         projectService: true,
-        project: ['tsconfig.json'],
         tsconfigRootDir: import.meta.dirname
       }
     },
