4fb7f62ba489b4b170a27bbedb0463e62a3a6f59
cipchk <cipchk@users.noreply.github.com>
2026-01-17 17:10:44 +0000

    Automated deployment: Sat Jan 17 17:10:44 UTC 2026 3c0c9e52930ae287751340a17cf1ccb7427e7720

---
+++ chunk-K7TNILLD.js
@@ -0,0 +1,1 @@
+import{U as o}from"./chunk-Q2G2QH7N.js";import{Ra as i,ea as n}from"./chunk-ULLXQYZ5.js";var y=(()=>{class e{srv=n(o);transform(r,t){return this.srv.format(r,t)}static \u0275fac=function(t){return new(t||e)};static \u0275pipe=i({name:"price",type:e,pure:!0})}return e})();export{y as a};
--- chunk-NDKULHPT.js
+++
@@ -1,1 +0,0 @@
-import{U as o}from"./chunk-WOQCCVFD.js";import{Ra as i,ea as n}from"./chunk-KNWXIIII.js";var y=(()=>{class e{srv=n(o);transform(r,t){return this.srv.format(r,t)}static \u0275fac=function(t){return new(t||e)};static \u0275pipe=i({name:"price",type:e,pure:!0})}return e})();export{y as a};
