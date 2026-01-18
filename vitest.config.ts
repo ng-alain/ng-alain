4c907021f9bfaba0b8f773c2ebbe93aae24ecaff
卡色 <cipchk@qq.com>
2026-01-18 14:48:44 +0800

    test: bump to vitest (#2598)

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    isolate: true,
+    testTimeout: 2000,
+    hookTimeout: 2000,
+    browser: {
+      screenshotFailures: false
+    }
+  }
+});
