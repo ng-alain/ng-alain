8d9f0600e856fbfa7689a60289a199ded4093379
cipchk <cipchk@users.noreply.github.com>
2026-03-26 12:48:20 +0000

    Automated deployment: Thu Mar 26 12:48:20 UTC 2026 a55e0db00d0ad6333c0d005e70e46b95292f96a0

---
+++ chunk-6L3LIYZW.js
@@ -0,0 +1,1 @@
+import{R as o}from"./chunk-K7GSPBPN.js";import{Ra as i,da as n}from"./chunk-MOBE5KQX.js";var y=(()=>{class e{srv=n(o);transform(r,t){return this.srv.format(r,t)}static \u0275fac=function(t){return new(t||e)};static \u0275pipe=i({name:"price",type:e,pure:!0})}return e})();export{y as a};
