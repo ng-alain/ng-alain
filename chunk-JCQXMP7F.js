0ebca03d46a14b2ea76546428b3515aec1ea177b
cipchk <cipchk@users.noreply.github.com>
2026-06-16 07:26:54 +0000

    deploy: 96e7a7f3a3067490a230e1aadc963d1e45bc012b

---
+++ chunk-JCQXMP7F.js
@@ -0,0 +1,1 @@
+import{R as o}from"./chunk-PQYXD3X4.js";import{Ra as i,da as n}from"./chunk-TMN5MCM4.js";var y=(()=>{class e{srv=n(o);transform(r,t){return this.srv.format(r,t)}static \u0275fac=function(t){return new(t||e)};static \u0275pipe=i({name:"price",type:e,pure:!0})}return e})();export{y as a};
